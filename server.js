eb8d1ac88008e43a97f5ae3f0b1a90b85ac997e9
Dan1elGun <91561829+Dan1elGun@users.noreply.github.com>
2023-06-19 00:32:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 app.use(express.static(__dirname))
-app.use(express.static(path.resolve(__dirname, 'public')))
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
